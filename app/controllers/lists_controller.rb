c27e852e5e169d09ab09bf9ac8eef2775b3bfa7e
Loubna Charrouf <charrouf.lb@gmail.com>
2021-10-29 12:43:05 +0100

    Heroku day

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,17 @@
+class ListsController < ApplicationController
+  def index
+    @list = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:create, :new, :show, :index] do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
 end
